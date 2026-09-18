1bbecd14aa1c92200af2240be3f98c35bb174b1b
vrijhwani <vross29@gmail.com>
2026-09-18 12:13:52 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.5
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "test"
+print(type(variable))
+variable = [10,2,2]
+print(type(variable))
