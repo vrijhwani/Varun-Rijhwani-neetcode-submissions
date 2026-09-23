c0233ce7f62fc739abc203876ca8070bdbf61548
vrijhwani <vross29@gmail.com>
2026-09-23 16:10:51 -0700

    Add: python-class-vs-instance-attributes - submission-0

---
+++ Python OOP/python-class-vs-instance-attributes/submission-0.py
@@ -0,0 +1,18 @@
+class BankAccount: 
+    # TODO: Add class and instance attributes at their appropriate places
+    total_accounts = 0
+    total_balance = 0
+    
+    def __init__(self, name: str, balance: float) :
+        self.name = name
+        self.balance = balance
+        BankAccount.total_accounts += 1
+        BankAccount.total_balance += balance
+
+alice = BankAccount("Alice", 1000)
+bob = BankAccount("Bob", 2000)
+
+print(f"Alice's balance: ${alice.balance}")
+print(f"Bob's balance: ${bob.balance}")
+print(f"Total Accounts: {BankAccount.total_accounts}")
+print(f"Total Balance: ${BankAccount.total_balance}")
